a855e28b828dba092c8bfd677d07bb72c7b83c50
VictorianoDL <Vitivitoriano@gmail.com>
2025-09-11 20:56:14 -0300

    Cambios 11/9

--- agape-FrontEnd/src/Components/Main/Home/Home.tsx
+++ agape-FrontEnd/src/Components/Main/Home/Home.tsx
@@ -1,13 +1,28 @@
 import Carrucel from "./Carrucel";
 import CasillaInfo from "./CasillaInfo";
 
+const image = [
+    "https://images.vexels.com/media/users/3/182371/isolated/preview/2f8c7e9f42c7781c3846b435475f92af-plano-de-fruta-de-manzana.png",
+    "https://images.vexels.com/media/users/3/182371/isolated/preview/2f8c7e9f42c7781c3846b435475f92af-plano-de-fruta-de-manzana.png",
+    "https://images.vexels.com/media/users/3/182371/isolated/preview/2f8c7e9f42c7781c3846b435475f92af-plano-de-fruta-de-manzana.png"
+];
+
+const descripcion = [
+    "aca va informancon de algo o sobre la aplicacions de doom y quierby para ciando que claor eso eheheheeeehh eso lo decia diego maradona y tambien tu puta madre",
+    "segunda descripcion"    
+];
+
 const Home = () => {
+
     return (
         <div className="home">
-
             <Carrucel />
-            <CasillaInfo />
-            <CasillaInfo />
+            <button>ir a campañas</button>
+            <div className="Casillas-Informacion">
+                <CasillaInfo imageUrl={image[1]} description={descripcion[0]} row={true}/>
+                <CasillaInfo imageUrl={image[2]} description={descripcion[1]} row={false} />
+            </div>
+            
             
         </div>
     );
