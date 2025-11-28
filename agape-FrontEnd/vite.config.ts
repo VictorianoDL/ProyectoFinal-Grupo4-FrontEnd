c911a90c6f84e489caf5f3703ea338dbf346c538
VictorianoDL <Vitivitoriano@gmail.com>
2025-11-27 21:17:33 -0300

    ultimo ultimo ultimo x2 commit (espero)

--- agape-FrontEnd/vite.config.ts
+++ agape-FrontEnd/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/auth': {
-        target: 'https://proyectofinal-grupo4-backend.onrender.com',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       },
