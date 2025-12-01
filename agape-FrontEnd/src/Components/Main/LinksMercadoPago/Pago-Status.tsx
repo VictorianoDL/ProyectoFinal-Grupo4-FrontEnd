3392e02a0b6b0544a5a8ac2fe1c935139d59cca5
VictorianoDL <Vitivitoriano@gmail.com>
2025-12-01 20:25:27 -0300

    algo

--- agape-FrontEnd/src/Components/Main/LinksMercadoPago/Pago-Status.tsx
+++ agape-FrontEnd/src/Components/Main/LinksMercadoPago/Pago-Status.tsx
@@ -2,32 +2,33 @@ import { useSearchParams, Link, useNavigate } from 'react-router-dom';
 
 const PagoStatus = () => {
     const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
 
     const paymentId = searchParams.get("payment_id");
     const status = searchParams.get("status");
 
-    // approved, rejected, pending
+    switch(status){
+        case "approved":{
+            return(
+                <div className="container-pago-status">
 
-    if(status === "approved"){
-        return (
-            <div className="container-pago-status">
+                    <h1>¡Gracias por tu aporte!</h1>
 
-                <h1>¡Gracias por tu aporte!</h1>
+                    <div className="detalles-compra">
+                        <p>El estado de tu pago es: <strong>{status}</strong></p>
+                        <p>Tu número de operación es: <strong>{paymentId}</strong></p>
+                    </div>
 
-                <div className="detalles-compra">
-                    <p>El estado de tu pago es: <strong>{status}</strong></p>
-                    <p>Tu número de operación es: <strong>{paymentId}</strong></p>
+                    <p>Te hemos enviado un comprobante a tu correo.</p>
+
+                    <Link to="/">Volver al inicio</Link>
+                    <Link to={"/campanias"}>Volver a Campañas</Link>
+
                 </div>
-
-                <p>Te hemos enviado un comprobante a tu correo.</p>
-
-                <Link to="/">Volver al inicio</Link>
-                <Link to={"/campanias"}>Volver a Campañas</Link>
-            </div>
-        );
-    }else{
-        if(status === "rejected" ){
-            return (
+            )
+        }
+        case "rejected":{
+            return(
                 <div className="container-pago-status">
 
                     <h1>Tu pago ha sido rechazado</h1>
@@ -41,21 +42,26 @@ const PagoStatus = () => {
                     <Link to={"/campanias"}>Volver a Campañas</Link>
 
                 </div>
-            );
-        }else{
-            <div className="container-pago-status">
-
-                <h1>Tu pago esta pendiente</h1>
-
-                <div className="detalles-compra">
-                    <p>El estado de tu pago es: <strong>{status}</strong></p>
-                    <p>Tu número de operación es: <strong>{paymentId}</strong></p>
-                </div>
-
-                <Link to="/">Volver al inicio</Link>
-                <Link to={"/campanias"}>Volver a Campañas</Link>
-            </div>
+            )
         }
+        case "in_process":{
+            return(
+                <div className="container-pago-status">
+
+                    <h1>Tu pago esta pendiente</h1>
+
+                    <div className="detalles-compra">
+                        <p>El estado de tu pago es: <strong>{status}</strong></p>
+                        <p>Tu número de operación es: <strong>{paymentId}</strong></p>
+                    </div>
+
+                    <Link to="/">Volver al inicio</Link>
+                    <Link to={"/campanias"}>Volver a Campañas</Link>
+
+                </div>     
+            )
+        }
+        default: navigate(`/campanias`)
     }
 };
 
